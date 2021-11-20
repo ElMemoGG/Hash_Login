33bffb238497f8c199c7646113aeb4b1434492ee
ElMemoGG <za17011262@zapopan.tecmm.edu.mx>
2021-11-20 14:19:07 -0600

    Initial commit

---
+++ app/src/main/java/com/maus/hash_login/Sesion.java
@@ -0,0 +1,18 @@
+package com.maus.hash_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Sesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sesion);
+    }
+    public void Atras(View view){
+
+    }
+}
